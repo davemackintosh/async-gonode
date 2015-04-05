51bb365bf099a52316fe9217b13ada9ad8b1fd38
davemackintosh <me@davemackintosh.co.uk>
2015-04-05 11:00:07 +0100

    WIP

---
+++ OO/oo.go
@@ -0,0 +1,26 @@
+package main
+
+import "log"
+
+type Animal struct {
+	Says string
+}
+
+type Cat struct {
+	*Animal
+}
+
+func (animal *Animal) Speak() string {
+	return animal.Says
+}
+
+func main() {
+	a_cat := Cat{&Animal{"Meow"}}
+	animal := new(Animal)
+
+	log.Print(a_cat)
+	log.Print(animal)
+
+	log.Print(a_cat.Speak())
+	// log.Print(animal.Speak())
+}
