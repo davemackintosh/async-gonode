39c986b4045e57371e38e6e881a79edf90f62463
davemackintosh <me@davemackintosh.co.uk>
2015-04-14 18:08:14 +0100

    Just some tidying up

--- Channels+Streams/stream.js
+++ Channels+Streams/stream.js
@@ -1,16 +1,19 @@
-var stream = require('stream'),
-  util = require('util')
+var stream = require('stream')
+var util = require('util')
 var Transform = stream.Transform
-var Reverse = function(options) {
+var Streamer = function(options) {
   Transform.call(this, options)
   this.text = []
 }
-util.inherits(Reverse, Transform)
-Reverse.prototype._transform = function(chunk, encoding, callback) {
+
+util.inherits(Streamer, Transform)
+
+Streamer.prototype._transform = function(chunk, encoding, callback) {
   this.text.push(chunk.toString('utf8'))
   callback()
 }
-Reverse.prototype._flush = function(callback) {
+
+Streamer.prototype._flush = function(callback) {
   this.text.forEach(function(text) {
     this.push(text, 'utf8')
   }.bind(this))
@@ -18,11 +21,12 @@ Reverse.prototype._flush = function(callback) {
 }
 
 //Here's how we use this stream
-var reverseStream = new Reverse()
-reverseStream.on('data', function(data) {
-    console.log('out: ', data.toString('utf8'))
+var outputStreamer = new Streamer()
+
+outputStreamer.on('data', function(data) {
+  console.log('out: ', data.toString('utf8'))
 })
 
-reverseStream.write('Hello ')
-reverseStream.write('World ')
-reverseStream.end()
+outputStreamer.write('Hello ')
+outputStreamer.write('World ')
+outputStreamer.end()
