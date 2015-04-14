e6df795a490dc00da49fdde850ff6f4fd6eb2035
davemackintosh <me@davemackintosh.co.uk>
2015-04-14 21:23:05 +0100

    Added comments

--- Native/cgo.go
+++ Native/cgo.go
@@ -7,7 +7,12 @@ import "C"
 import "unsafe"
 
 func main() {
+	// Get a pointer to a []char*
 	cs := C.CString("Hello from Go and C\n")
+
+	// Say hello.
 	C.sayhello(cs)
+
+	// Use that pointer to free memory, this is C..
 	C.free(unsafe.Pointer(cs))
 }
