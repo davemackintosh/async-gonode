f4bacf6e7927016435bc27bfe9848925ede13523
davemackintosh <me@davemackintosh.co.uk>
2015-04-15 11:33:33 +0100

    Tidied up

--- Native/cgo.go
+++ Native/cgo.go
@@ -1,18 +1,17 @@
 package main
 
-/*
-#include "sayhello.c"
-*/
+//#include "sayhello.c"
 import "C"
 import "unsafe"
 
 func main() {
-	// Get a pointer to a []char*
+	// Get a pointer to a []char.
 	cs := C.CString("Hello from Go and C\n")
 
 	// Say hello.
 	C.sayhello(cs)
 
-	// Use that pointer to free memory, this is C..
+	// Strings are allocated with malloc so we need
+	// to manually free that memory or face the consequences..
 	C.free(unsafe.Pointer(cs))
 }
